0267170e162abeec4f35052938a31ec2635bad80
Hiba T <hiba.glory@gmail.com>
2023-10-21 10:45:43 -0500

    Publish Sensor Data to ROS #8

--- rpi/robot_bringup/robot_bringup/adafruit_imu.py
+++ rpi/robot_bringup/robot_bringup/adafruit_imu.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_mpu6050
@@ -10,8 +7,12 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    #print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
     print("Temperature: %.2f C" % mpu.temperature)
-    print("")
+    #print("")
+    x_acc = ("%.2f,%.2f,%.2f" % (mpu.acceleration))
+    #x_acc = "Acceleration: X:%.2f" % (mpu.acceleration)
+    print(mpu.acceleration)
+    print(x_acc)
     time.sleep(1)
